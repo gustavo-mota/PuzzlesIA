34e1943142f5a6e120ca007d78eebedcd1598878
Gustavo1001 <gustavomotax2@gmail.com>
2019-11-10 17:26:21 -0300

    Tempera modify

--- NQueens/Tempera/TemperaSimulada.py
+++ NQueens/Tempera/TemperaSimulada.py
@@ -7,45 +7,67 @@ import copy
 size = 40
 
 def solution(matriz):
-    lines = []
-    for i in range(len(matriz)):
-        lines.append([0] * len(matriz))
-    for j in range(len(matriz)):
-        lines[matriz[j]][j] = 1
-    for k in range(len(matriz)):
-        print(lines[k])
+    for i in matriz:
+        print(list(i))
 
 
 def sort_change(matriz):  # array
-    column_one = random.randint(0, matriz.size - 1)
-    column_two = random.randint(0, matriz.size - 1)
+	matriz_change = matriz.copy()
+    numbers = list(range(0, len(matriz)))
+    column_one = random.choice(numbers)
+    numbers.remove(column_one)
+    column_two = random.choice(numbers)
     # alteração das colunas
-    var = matriz[column_one]
-    matriz[column_one] = matriz[column_two]
-    matriz[column_two] = var
-    return matriz
+    var = numpy.copy(matriz_change[:, column_one])
+    matriz_change[:, column_one] = matriz_change[:, column_two]
+    matriz_change[:, column_two] = var
+    return matriz_change
 
 
 def cost_collision(matriz, size):  # array
     atack = 0
-    copia = numpy.copy(matriz)
-    for i in range(0, matriz.size):
-        for j in range(i + 1, copia.size):
-            if abs(i - j) == abs(matriz[i] - copia[j]):
+    for i in range(size):
+        line_atcolumn = list(matriz[:, i]).index(1)
+        for j in range(i+1, size):
+            if abs(i - j) == abs(line_atcolumn - list(matriz[:,j ]).index(1)):
                 atack += 1
     return atack
 
 
 def initial_generate(size):  # array
-    return numpy.array(range(0, size))
+    return numpy.identity(size)
 
 
 
 def explorer(size):
+    lista = [initial_generate(size)]
+    solved = False
+    while True:
+        for i in range(len(lista)):
+            current = lista[i]
+            current_collision = cost_collision(current, size)
+            new = sort_change(current)
+            newest_collision = cost_collision(new, size)
+            if newest_collision == 0:
+                solution(new)
+                solved = True
+                break
+            elif newest_collision < current_collision:
+                lista[i] = new.copy()
+            else:
+                probability = math.e**(current_collision - newest_collision)
+                rand = random.uniform(0, 1)
+                if rand <= probability:
+                    print("aqui")
+                    lista.append(new)
+        if solved:
+            break
+
+'''def explorer(size):
     current = initial_generate(size)
-    for i in range(50000):
-        new = sort_change(current)
+    while True:
         current_collision = cost_collision(current, size)
+        new = sort_change(current)
         newest_collision = cost_collision(new, size)
         print(current_collision, newest_collision)
         if newest_collision == 0:
@@ -57,8 +79,6 @@ def explorer(size):
             probability = math.exp(current_collision - newest_collision)
             rand = random.uniform(0, 1)
             if rand <= probability:
-                current = new.copy()
-    print(newest_collision)
-    solution(new)
+                current = new.copy()'''
 
 explorer(size)
